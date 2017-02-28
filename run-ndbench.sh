4f415e872c67f737e8818a9a9e427b54ede35a48
diegopacheco <diego.pacheco.it@gmail.com>
2017-02-28 11:58:33 -0300

    Remove duplicate pid and add print for seeds

--- run-ndbench.sh
+++ run-ndbench.sh
@@ -5,8 +5,7 @@ export DYNOMITE_SEEDS="127.0.0.1:8102:rack1:dc:100";
 ./gradlew farmStartWar > ndbench-log.log  2>&1 &
 sleep 3
 
-PID="PID :  $(pgrep java)"
-echo $PID
-echo $PID >> ndbench-log.log
+INFO="Running NDbench with PID:  $(pgrep java) SEEDS: $DYNOMITE_SEEDS"
+echo $INFO >> ndbench-log.log
 
 tail -f ndbench-log.log
