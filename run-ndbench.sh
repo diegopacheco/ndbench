39cbf1010120410354fde8da9669f7fb39a24192
diegopacheco <diego.pacheco.it@gmail.com>
2017-02-28 11:32:37 -0300

    fix pid detection and outputs

--- run-ndbench.sh
+++ run-ndbench.sh
@@ -2,8 +2,8 @@
 
 export DYNOMITE_SEEDS="127.0.0.1:8101:rack1:100:dc";
 
-./gradlew farmStartWar > ndbench-log.log &
-PID="PID :  $(pgrep gradlew) "
+./gradlew farmStartWar > ndbench-log.log  2>&1 &
+PID="PID :  $(pgrep java)"
 echo $PID
 echo $PID >> ndbench-log.log
 
