656ac4aa89ac8598e7dab9048df202d5beb095c8
diegopacheco <diego.pacheco.it@gmail.com>
2017-02-28 11:45:27 -0300

    Fix the seeds formart and PID

--- run-ndbench.sh
+++ run-ndbench.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 
-export DYNOMITE_SEEDS="127.0.0.1:8101:rack1:100:dc";
+export DYNOMITE_SEEDS="127.0.0.1:8102:rack1:dc:100";
 
 ./gradlew farmStartWar > ndbench-log.log  2>&1 &
+sleep 3
+
 PID="PID :  $(pgrep java)"
 echo $PID
 echo $PID >> ndbench-log.log
