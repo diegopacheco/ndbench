96a9b251142257e9e377c497589fa0077216dcdd
diegopacheco <diego.pacheco.it@gmail.com>
2017-02-28 11:29:14 -0300

    added proper settings to run script

--- run-ndbench.sh
+++ run-ndbench.sh
@@ -1,3 +1,10 @@
 #/bin/bash
 
+export DYNOMITE_SEEDS="127.0.0.1:8101:rack1:100:dc";
+
 ./gradlew farmStartWar > ndbench-log.log &
+PID="PID :  $(pgrep gradlew) "
+echo $PID
+echo $PID >> ndbench-log.log
+
+tail -f ndbench-log.log
